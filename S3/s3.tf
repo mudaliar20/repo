d53ae9a71d83055b46ba5b64548bf49504749bb3
mudaliar20 <54627121+mudaliar20@users.noreply.github.com>
2020-07-24 00:56:34 +0530

    Update s3.tf

--- S3/s3.tf
+++ S3/s3.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-   region = "us-east-2"
-   assume_role {
-	role_arn = "arn:aws:iam::759442462106:role/terraform-test-role"
+  region     = "us-east-2"
+  access_key = "AKIA3BUSMKWNMS7LXN77"
+  secret_key = "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"
 }
-}
+
 
 resource "aws_s3_bucket" "s3bucket" {
   bucket = "kaiburr-test3"
